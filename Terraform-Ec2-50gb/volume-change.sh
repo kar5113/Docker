4195713553c5d60f6615f752cd05a83d2df7fb28
karthikeyareddyankireddy <karthikeya.ankireddy@rabobank.nl>
2025-12-07 15:05:41 +0530

    df

--- Terraform-Ec2-50gb/volume-change.sh
+++ Terraform-Ec2-50gb/volume-change.sh
@@ -2,3 +2,20 @@
 growpart /dev/nvme0n1 4
 lvextend -L +30G /dev/mapper/RootVG-varVol
 xfs_growfs /var
+
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine \
+                  podman \
+                  runc
+
+sudo dnf -y install dnf-plugins-core 
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl enable docker  
+sudo usermod -aG docker ec2-user               
